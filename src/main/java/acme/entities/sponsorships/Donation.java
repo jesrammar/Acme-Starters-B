101f3034d81ad23d71f73d8a323b032720574df0
josegicar <josegicar@alum.us.es>
2026-02-14 05:23:03 +0100

    feat: create data model for sponsorships

---
+++ src/main/java/acme/entities/sponsorships/Donation.java
@@ -0,0 +1,52 @@
+
+package acme.entities.sponsorships;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.datatypes.Money;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidMoney;
+import acme.constraints.ValidHeader;
+import acme.constraints.ValidText;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Donation extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@ValidHeader
+	@Column
+	private String				name;
+
+	@Mandatory
+	@ValidText
+	@Column
+	private String				notes;
+
+	@Mandatory
+	@ValidMoney(min = 0.0)
+	@Column
+	private Money				money;
+
+	@Mandatory
+	@Enumerated(EnumType.STRING)
+	@Column
+	private DonationKind		kind;
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Sponsorship			sponsorship;
+
+}
---
+++ src/main/java/acme/realms/Sponsor.java
@@ -0,0 +1,37 @@
+
+package acme.realms;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractRole;
+import acme.client.components.validation.Mandatory;
+import acme.constraints.ValidHeader;
+import acme.constraints.ValidText;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Sponsor extends AbstractRole {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@Mandatory
+	@ValidText
+	@Column
+	private String				address;
+
+	@Mandatory
+	@ValidHeader
+	@Column
+	private String				im;
+
+	@Mandatory
+	@Valid
+	@Column
+	private Boolean				gold;
+
+}
