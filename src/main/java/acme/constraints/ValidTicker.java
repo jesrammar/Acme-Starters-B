6873fd0982935e0f84b1b57a08efb1b3cf824bbc
Francisco Manuel Sabido González <frasabgon@alum.us.es>
2026-02-20 19:10:08 +0100

    fix: arreglo en los validadores grupales tras darlos en clase

--- src/main/java/acme/constraints/ValidTicker.java
+++ src/main/java/acme/constraints/ValidTicker.java
@@ -1,6 +1,6 @@
+
 package acme.constraints;
 
-import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -8,23 +8,16 @@ import java.lang.annotation.Target;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
 
-@Documented
-@Constraint(validatedBy = {})
 @Target({
-	ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.ANNOTATION_TYPE
+	ElementType.FIELD
 })
 @Retention(RetentionPolicy.RUNTIME)
-@NotBlank
-@Pattern(regexp = "^[A-Z]{2}[0-9]{2}-\\w{5,10}$")
+@Constraint(validatedBy = ValidTickerValidator.class)
+
 public @interface ValidTicker {
 
-	String message() default "{acme.validation.validTicker.message}";
-
+	String message() default "{acme.validation.valid-ticker}";
 	Class<?>[] groups() default {};
-
 	Class<? extends Payload>[] payload() default {};
-
 }
