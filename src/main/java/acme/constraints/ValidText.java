1c1dc51531ecfe94e301feebbf737fb4f8112f73
Rares Nicolae Petre <rarpet@alum.us.es>
2026-02-24 13:04:48 +0100

    hotfix: Refactored groupal Validators

--- src/main/java/acme/constraints/ValidText.java
+++ src/main/java/acme/constraints/ValidText.java
@@ -3,19 +3,20 @@ package acme.constraints;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
 import java.lang.annotation.RetentionPolicy;
-
+import java.lang.annotation.Target;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
 
+import org.hibernate.validator.constraints.Length;
+
 @Target({
-	ElementType.FIELD
+	ElementType.FIELD, ElementType.METHOD
 })
 @Retention(RetentionPolicy.RUNTIME)
-@Constraint(validatedBy = ValidTextValidator.class)
-
+@Constraint(validatedBy = {})
+@Length(min = 1, max = 255)
 public @interface ValidText {
 
 	String message() default "{acme.validation.valid-text}";
