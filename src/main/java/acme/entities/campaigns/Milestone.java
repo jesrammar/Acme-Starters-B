d3a0083d8a430239046194a966e160cf683912e9
jesrammar <jesrammar2@alum.us.es>
2026-03-02 10:29:46 +0100

    feat(s2): campaigns model + validators + sample/errata data

---
+++ src/main/java/acme/entities/campaigns/Milestone.java
@@ -0,0 +1,51 @@
+package acme.entities.campaigns;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+
+import acme.client.components.basis.AbstractEntity;
+import acme.client.components.validation.Mandatory;
+import acme.client.components.validation.ValidNumber;
+import acme.constraints.ValidHeader;
+import acme.constraints.ValidText;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Milestone extends AbstractEntity {
+
+
+	@Mandatory
+	@ValidHeader
+	@Column
+	private String title;
+
+	@Mandatory
+	@ValidText
+	@Column
+	private String achievements;
+
+	@Mandatory
+	@ValidNumber(min = 0.01, fraction = 2)
+	@Column
+	private Double effort;   
+
+	@Mandatory
+	@Valid
+	@Column
+	private MilestoneKind kind;
+
+	
+
+	@Mandatory
+	@Valid
+	@ManyToOne(optional = false)
+	private Campaign campaign;
+
+}
