a5c7f4b38e27210833bac84d74170b1f72b487e8
Francisco Manuel Sabido González <frasabgon@alum.us.es>
2026-03-03 18:21:15 +0100

    feat: mostrar y listar auditReports

--- src/main/java/acme/features/auditor/auditReport/AuditReportController.java
+++ src/main/java/acme/features/auditor/auditReport/AuditReportController.java
@@ -1,6 +1,28 @@
+
 package acme.features.auditor.auditReport;
 
+import javax.annotation.PostConstruct;
 
-public class AuditReportController {
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
 
+import acme.client.controllers.AbstractController;
+import acme.entities.audit.AuditReport;
+import acme.realms.Auditor;
+
+@Controller
+public class AuditReportController extends AbstractController<Auditor, AuditReport> {
+
+	// Constructors -----------------------------------------------------------
+
+	@PostConstruct
+	protected void initialise() {
+		super.setMediaType(MediaType.TEXT_HTML);
+
+		super.addBasicCommand("list", AuditReportListService.class);
+		super.addBasicCommand("show", AuditReportShowService.class);
+		//	super.addBasicCommand("create", AuditReportCreateService.class);
+		//	super.addBasicCommand("update", AuditReportUpdateService.class);
+		//	super.addBasicCommand("delete", AuditReportDeleteService.class);
+	}
 }
--- src/main/java/acme/features/auditor/auditReport/AuditReportListService.java
+++ src/main/java/acme/features/auditor/auditReport/AuditReportListService.java
@@ -1,6 +1,57 @@
+
 package acme.features.auditor.auditReport;
 
+import java.util.Collection;
 
-public class AuditReportListService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import acme.client.components.models.Tuple;
+import acme.client.services.AbstractService;
+import acme.entities.audit.AuditReport;
+import acme.realms.Auditor;
+
+@Service
+public class AuditReportListService extends AbstractService<Auditor, AuditReport> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	private AuditReportRepository	repository;
+
+	private Collection<AuditReport>	auditReports;
+
+	// AbstractService interface ----------------------------------------------
+
+
+	@Override
+	public void load() {
+		int userAccountId;
+
+		userAccountId = super.getRequest().getPrincipal().getAccountId();
+
+		this.auditReports = (Collection<AuditReport>) this.repository.findAuditReportsByAuditorId(userAccountId);
+	}
+
+	@Override
+	public void authorise() {
+		super.setAuthorised(true);
+	}
+
+	@Override
+	public void unbind() {
+
+		for (AuditReport auditReport : this.auditReports) {
+
+			Tuple tuple;
+
+			tuple = super.unbindObject(auditReport, "ticker", "name", "startMoment", "endMoment", "draftMode");
+
+			tuple.put("hours", auditReport.getHours());
+
+			long sectionCount = this.repository.countSectionsByReportId(auditReport.getId());
+
+			tuple.put("sectionCount", sectionCount);
+		}
+	}
 }
