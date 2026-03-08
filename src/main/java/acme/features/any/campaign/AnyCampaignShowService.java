34ea8a3ffb55e12f7e771534016519c70e87b855
jesrammar <jesrammar2@alum.us.es>
2026-03-08 12:27:52 +0100

    feat(s2): campaign any views and sample campaign coverage

---
+++ src/main/java/acme/features/any/campaign/AnyCampaignShowService.java
@@ -0,0 +1,42 @@
+package acme.features.any.campaign;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.client.components.models.Tuple;
+import acme.client.components.principals.Any;
+import acme.client.services.AbstractService;
+import acme.entities.campaigns.Campaign;
+import acme.entities.campaigns.CampaignRepository;
+
+@Service
+public class AnyCampaignShowService extends AbstractService<Any, Campaign> {
+
+	@Autowired
+	private CampaignRepository		repository;
+
+	private Campaign				campaign;
+
+	@Override
+	public void load() {
+		int id;
+
+		id = super.getRequest().getData("id", int.class);
+		this.campaign = this.repository.findPublishedCampaignById(id);
+	}
+
+	@Override
+	public void authorise() {
+		super.setAuthorised(this.campaign != null);
+	}
+
+	@Override
+	public void unbind() {
+		Tuple tuple;
+
+		tuple = super.unbindObject(this.campaign, "ticker", "name", "description", "startMoment", "endMoment", "moreInfo");
+		tuple.put("monthsActive", this.campaign.getMonthsActive());
+		Double effort = this.repository.computeCampaignEffort(this.campaign.getId());
+		tuple.put("effort", effort == null ? 0.0 : effort);
+	}
+}
