7fb1a87cc9a0b3c2cbacb3e8de372a3af2a6b917
FcoManuelSabidoGonzalez <frasabgon@alum.us.es>
2026-03-11 19:16:54 +0100

    feat: create audit report complete

--- src/main/java/acme/features/auditor/auditReport/AuditReportCreateService.java
+++ src/main/java/acme/features/auditor/auditReport/AuditReportCreateService.java
@@ -1,6 +1,52 @@
+
 package acme.features.auditor.auditReport;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public class AuditReportCreateService {
+import acme.client.services.AbstractService;
+import acme.entities.audit.AuditReport;
+import acme.realms.Auditor;
 
+@Service
+public class AuditReportCreateService extends AbstractService<Auditor, AuditReport> {
+
+	@Autowired
+	private AuditReportRepository	repository;
+
+	private AuditReport				auditReport;
+
+
+	@Override
+	public void load() {
+		Auditor auditor = (Auditor) super.getRequest().getPrincipal().getActiveRealm();
+		this.auditReport = super.newObject(AuditReport.class);
+		this.auditReport.setAuditor(auditor);
+		this.auditReport.setDraftMode(true);
+	}
+
+	@Override
+	public void authorise() {
+		super.setAuthorised(true);
+	}
+
+	@Override
+	public void bind() {
+		super.bindObject(this.auditReport, "ticker", "name", "description", "startMoment", "endMoment", "moreInfo");
+	}
+
+	@Override
+	public void validate() {
+		super.validateObject(this.auditReport);
+	}
+
+	@Override
+	public void execute() {
+		this.repository.save(this.auditReport);
+	}
+
+	@Override
+	public void unbind() {
+		super.unbindObject(this.auditReport, "ticker", "name", "description", "startMoment", "endMoment", "moreInfo", "draftMode");
+	}
 }
