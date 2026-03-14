e60315b82c03d9545b97e56e2a298484bb3494fa
Rares Nicolae Petre <rarpet@alum.us.es>
2026-03-14 17:28:57 +0100

    hotfix: Changed querys for returning parts associated to PUBLISHED inventions

--- src/main/java/acme/features/any/inventor/part/AnyPartRepository.java
+++ src/main/java/acme/features/any/inventor/part/AnyPartRepository.java
@@ -12,19 +12,19 @@ import acme.entities.inventions.Part;
 @Repository
 public interface AnyPartRepository extends AbstractRepository {
 
-	@Query("select p from Part p where p.id = :partId and p.invention.inventor.id = :inventorId and p.invention.draftMode = true")
+	@Query("select p from Part p where p.id = :partId and p.invention.inventor.id = :inventorId and p.invention.draftMode = false")
 	Part findPublishedPartByIdAndInventorId(int partId, int inventorId);
 
-	@Query("select p from Part p where p.id = :partId and p.invention.draftMode = true")
+	@Query("select p from Part p where p.id = :partId and p.invention.draftMode = false")
 	Part findPublishedPartById(int partId);
 
-	@Query("select p from Part p where p.invention.inventor.id = :inventorId and p.invention.draftMode = true")
+	@Query("select p from Part p where p.invention.inventor.id = :inventorId and p.invention.draftMode = false")
 	Collection<Part> findPublishedPartsByInventorId(int inventorId);
 
 	@Query("select p from Part p where p.invention.id = :inventionId")
 	Collection<Part> findPublishedPartsByInventionId(int inventionId);
 
-	@Query("select p from Part p where p.invention.id = :inventionId and p.invention.inventor.id = :inventorId and p.invention.draftMode = true")
+	@Query("select p from Part p where p.invention.id = :inventionId and p.invention.inventor.id = :inventorId and p.invention.draftMode = false")
 	Collection<Part> findPublishedPartsByInventionIdAndInventorId(int inventionId, int inventorId);
 
 }
