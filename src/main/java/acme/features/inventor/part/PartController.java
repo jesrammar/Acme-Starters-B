34f84bd63ce3aa5c325ce4822c7f0d51b502486a
Rares Nicolae Petre <rarpet@alum.us.es>
2026-03-14 15:10:14 +0100

    feat: Added classes for Inventor Part Listing

---
+++ src/main/java/acme/features/inventor/part/PartController.java
@@ -0,0 +1,25 @@
+
+package acme.features.inventor.part;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+
+import acme.client.controllers.AbstractController;
+import acme.entities.inventions.Part;
+import acme.realms.Inventor;
+
+@Controller
+public class PartController extends AbstractController<Inventor, Part> {
+
+	// Constructors
+
+	@PostConstruct
+	protected void initialise() {
+		super.setMediaType(MediaType.TEXT_HTML);
+
+		super.addBasicCommand("list", PartListService.class);
+	}
+
+}
